5870efd345edaedcc4fbb37b2a20fa89b043ce4d
Rubiksron <rubiksron@gmail.com>
2019-09-04 17:34:44 -0700

    i can delete and create new todos. set up axios to do http requests and now i need to set up a db to persist the data.

--- todo-list-app/src/App.js
+++ todo-list-app/src/App.js
@@ -1,33 +1,27 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
 import Header from './components/layout/Header';
-import uuid from 'uuid';
+import About from './components/pages/About';
+// import uuid from 'uuid';
+import axios from 'axios';
+
 import './App.css';
 
 class App extends Component {
   state = {
-    todos: [
-      {
-        id: uuid.v4(),
-        title: 'Take dog for walk',
-        completed: false
-      },
-      {
-        id: uuid.v4(),
-        title: 'Dinner with racoons',
-        completed: false
-      },
-      {
-        id: uuid.v4(),
-        title: 'Meeting with dolphins',
-        completed: false
-      },
-    ]
+    todos: []
+  }
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+    .then(res => this.setState({ todos:res.data }));
   }
 
   deleteTodo = (id) => {
-    this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] })
+    axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+      .then(res => this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] }))
+
   }
 
   markComplete = (id) => {
@@ -41,23 +35,34 @@ class App extends Component {
   }
 
   addTodo = (title) => {
-    const newTodo = {
-      id: uuid.v4(),
-      title: title,
+    axios.post('https://jsonplaceholder.typicode.com/todos', {
+      title:title,
       completed: false
-    }
-    this.setState({ todos: [...this.state.todos, newTodo]})
+    })
+    .then(res => this.setState({ todos: [...this.state.todos, res.data]}))
+    .catch(err => (err))
   }
 
   render() {
     return (
-      <div className="App">
-        <div className="container">
-          <Header />
-          <AddTodo addTodo={ this.addTodo }/>
-          <Todos deleteTodo={ this.deleteTodo } todos={ this.state.todos } markComplete={ this.markComplete }/>
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header />
+            <Route exact path="/" render={ props => (
+              <React.Fragment>
+              <AddTodo addTodo={ this.addTodo }/>
+              <Todos
+              todos={ this.state.todos }
+              markComplete={ this.markComplete }
+              deleteTodo={ this.deleteTodo }
+              />
+              </React.Fragment>
+            )}/>
+            <Route path="/about" component={ About  } />
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
